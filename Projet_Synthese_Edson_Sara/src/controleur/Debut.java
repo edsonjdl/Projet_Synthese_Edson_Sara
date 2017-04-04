6177cf082c9bceb540a7d746cbbe49159d83ce86
edsonjdl <edsonjl@hotmail.com>
2017-04-04 12:49:55 -0400

    inclure biblioteques dans le projet

--- Projet_Synthese_Edson_Sara/src/controleur/Debut.java
+++ Projet_Synthese_Edson_Sara/src/controleur/Debut.java
@@ -5,20 +5,21 @@
  */
 package controleur;
 
-import connexionDB.DaoCotisation;
-import donneesAffichage.CreationDonneesMM;
-import donneesAffichage.DonneesAffichageMM;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import connexionDB.DaoCotisation;
+import donneesAffichage.CreationDonneesMM;
+import donneesAffichage.DonneesAffichageMM;
 import modele.Cotisation;
 import modele.ListeCotisations;
 
